b49ee4a6eea0775b0e1d9c4370cff49cf83d8338
Romesh Abeyawardena <romesh.a@outlook.com>
2024-09-14 13:36:19 +0100

    wip

---
+++ backend/src/Domain/JournalViewer.Domain/Extensions/NotifiableEntityBaseExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace JournalViewer.Domain.Extensions;
+
+public static class NotifiableEntityBaseExtensions
+{
+    public static async Task<string> PrepareAsJsonAsync<T>(this T item, 
+        CancellationToken cancellationToken, JsonSerializerOptions? jsonSerializerOptions = null)
+        where T : NotifiableEntityBase<T>
+    {
+        using var memoryStream = new MemoryStream();
+        await JsonSerializer.SerializeAsync(memoryStream, item,
+            jsonSerializerOptions, cancellationToken);
+        return Encoding.UTF8.GetString(memoryStream.ToArray());
+    }
+}
---
+++ backend/src/Infrastructure/JournalViewer.Infrastructure.Domain/Models/DbArticle.cs
@@ -0,0 +1,27 @@
+﻿using JournalViewer.Domain;
+using JournalViewer.Domain.Extensions;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace JournalViewer.Infrastructure.Domain.Models;
+
+public class DbArticle : NotifiableEntityBase<DbArticle>
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string? Summary { get; set; }
+    //Do not include this in the notification - it may be too large, consumers will be able to query the API using the Key deduced from GetKey or EF Db Identity
+    [JsonIgnore]
+    public string? Content { get; set; }
+
+    public override TKey GetKey<TKey>(DbArticle model)
+    {
+        return (TKey)(object)model.Id;
+    }
+
+    public override async Task<string> PrepareNotificationAsync(DbArticle result, NotificationType notificationType, CancellationToken cancellationToken)
+    {
+        return await this.PrepareAsJsonAsync(cancellationToken);
+    }
+}
